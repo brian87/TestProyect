f8fb9216c087b612886100b398362f969eb3ce84
Fermin <fermin.aguilar.b@gmail.com>
2019-06-27 13:04:52 -0500

    Main

--- SimulationVentasProductos/src/package1/TradeSimulator.java
+++ SimulationVentasProductos/src/package1/TradeSimulator.java
@@ -1,9 +1,15 @@
 package package1;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class TradeSimulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Company> companies = new ArrayList<Company>();
+			
 		Company BIGA = new Company("A");
 		Company BIGB = new Company("B");
 		
