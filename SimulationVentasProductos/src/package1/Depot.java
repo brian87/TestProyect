aebc83ba801992a817ba0328f474b40550943d4c
Fermin <fermin.aguilar.b@gmail.com>
2019-06-27 15:33:16 -0500

    Modificando clase Producto

--- SimulationVentasProductos/src/package1/Depot.java
+++ SimulationVentasProductos/src/package1/Depot.java
@@ -1,22 +1,8 @@
 package package1;
 
 public class Depot{
-	private int priceProduct;
-	private int priceDelivery;
 	
-	public int getPriceProduct() {
-		priceProduct = (int)(Math.random()*10+1);
-		return priceProduct;
-	}
-	public void setPriceProduct(int priceProduct) {
-		this.priceProduct = priceProduct;
-	}
-	public int getPriceDelivery() {
-		priceProduct = (int)(Math.random()*10+1);
-		return priceDelivery;
-	}
-	public void setPriceDelivery(int priceDelivery) {
-		this.priceDelivery = priceDelivery;
-	}
+	
+	
 
 }
