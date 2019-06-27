c9b0500762126e65958de7313f381d8ccbaafbf0
Fermin <fermin.aguilar.b@gmail.com>
2019-06-27 12:16:22 -0500

    creando Instancias

--- SimulationVentasProductos/src/package1/TradeSimulator.java
+++ SimulationVentasProductos/src/package1/TradeSimulator.java
@@ -4,6 +4,11 @@ public class TradeSimulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Company BIGA = new Company("A");
+		Company BIGB = new Company("B");
+		
+		System.out.print(BIGA.getName());
+		
 
 	}
 
