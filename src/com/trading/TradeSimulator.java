7f19ca59a099b32c1dc0191096ae48920a8dacb7
Christian Aguilar <xtian.aguilar.b@gmail.com>
2019-07-04 21:20:00 +0100

    Transactions

--- src/com/trading/TradeSimulator.java
+++ src/com/trading/TradeSimulator.java
@@ -21,6 +21,7 @@ public class TradeSimulator {
 		TradeSimulator tradeSimulator = new TradeSimulator();
 		tradeSimulator.init();
 		tradeSimulator.show();
+		tradeSimulator.simulateTransactions();
 	}
 
 	private void init() {
@@ -43,7 +44,7 @@ public class TradeSimulator {
 		for (int i = 1; i <= companyA.getMaxNroDepots(); i++) {
 			Double allowance = generateRandomDouble(MIN_ALLOWANCE_FOR_DEPOT, MAX_ALLOWANCE_FOR_DEPOT);
 			Depot depot = new Depot(companyA, "Depot-" + i, allowance);
-			// Stock for Depot			
+			// Stock for Depot
 			Integer quantity = generateRandomInteger(MIN_NATIVE_PRODUCT_QUANTITY, MAX_NATIVE_PRODUCT_QUANTITY);
 			Double productPrice = generateRandomDouble(MIN_PRODUCT_PRICE, MAX_PRODUCT_PRICE);
 			Double deliveryPrice = generateRandomDouble(MIN_PRODUCT_PRICE, MAX_PRODUCT_PRICE);
@@ -54,24 +55,31 @@ public class TradeSimulator {
 		for (int i = 1; i <= companyB.getMaxNroDepots(); i++) {
 			Double allowance = generateRandomDouble(MIN_ALLOWANCE_FOR_DEPOT, MAX_ALLOWANCE_FOR_DEPOT);
 			Depot depot = new Depot(companyB, "Depot-" + i, allowance);
-			// Stock for Depot			
+			// Stock for Depot
 			Integer quantity = generateRandomInteger(MIN_NATIVE_PRODUCT_QUANTITY, MAX_NATIVE_PRODUCT_QUANTITY);
 			Double productPrice = generateRandomDouble(MIN_PRODUCT_PRICE, MAX_PRODUCT_PRICE);
 			Double deliveryPrice = generateRandomDouble(MIN_PRODUCT_PRICE, MAX_PRODUCT_PRICE);
 			depot.addStock(new Stock(productB, Stock.Type.NATIVE, quantity, productPrice, deliveryPrice));
 			companyB.addDepot(depot);
 		}
-		
-		//transaccion
+
+	}
+
+	private void simulateTransactions() {
+		// transaccions
 		Transaccion transaccion = new Transaccion();
-		transaccion.setSellerDepot(productA.getProducer().getDepots().get(0));
-		System.out.println("Seller " + transaccion.getSellerDepot().getCompany().getName()+ " "+
-							transaccion.getSellerDepot().getName());
-		
-		
-		transaccion.setBuyerDepot(productB.getProducer().getDepots().get(0));
-		System.out.println("Buyer " + transaccion.getBuyerDepot().getCompany().getName()+ " "+
-				transaccion.getSellerDepot().getName());		
+		Depot buyerDepot = getRandomDepot(companies.get(0).getDepots());
+		Depot sellerDepot = getRandomDepot(companies.get(1).getDepots());
+		transaccion.setBuyerDepot(buyerDepot);
+		transaccion.setSellerDepot(sellerDepot);
+		System.out.println("Buyer " + transaccion.getBuyerDepot());
+		System.out.println("Seller " + transaccion.getSellerDepot());
+
+	}
+
+	private Depot getRandomDepot(List<Depot> depots) {
+		Random random = new Random();
+		return depots.get(random.nextInt(depots.size()));
 	}
 
 	private Double generateRandomDouble(Double min, Double max) {
